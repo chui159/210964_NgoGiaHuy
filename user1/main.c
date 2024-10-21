4633fd52d2f434ff0cbc0572617f0e193965bcec
chui159 <huyngogia123@gmail.com>
2024-10-21 15:42:27 +0700

    Add shellc1.c shellc2.c main.c ThucThi

---
+++ user1/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void inShellc1();
+void inShellc2();
+int main()
+{
+	inShellc1();
+	inShellc2();
+	return 0;
+}
---
+++ user1/shellc1.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+void inShellc1(){
+	printf("This is shellc1\n");
+}
